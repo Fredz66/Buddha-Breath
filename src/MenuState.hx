2bcfe24d6d34d5ad988bcdd14c3c4b9fe4c3756c
Fredz66 <frederic.lopez@free.fr>
2019-05-31 18:51:57 +0200

    Affiche le bouton plein écran uniquement pour la plateforme html5.

--- src/MenuState.hx
+++ src/MenuState.hx
@@ -26,8 +26,10 @@ class MenuState extends FlxState
 		add(new FlxButton(280, 200, "Settings", settings));
 		add(new FlxButton(280, 220, "Quit", quit));
 
+		#if html5
 		var button = new FlxButton(10, 10, "Fullscreen", function() FlxG.fullscreen = !FlxG.fullscreen);
 		add(button);
+		#end
 
 		super.create();
 
